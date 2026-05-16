254e29f8c733ae28abdc95851d16945912b9aae8
Katargina <katrin-kate@ukr.net>
2026-05-16 19:48:37 +0200

    ???

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { Note, NoteTag } from "../types/note";
+import type { Note, NoteTag } from "../types/temps";
 
 const BASE_URL = "https://notehub-public.goit.study/api";
 
